ff4ef84afb3fec32cbcb7863a5e51a9f20bb8435
kristofkuktin <144907617+kristofkuktin@users.noreply.github.com>
2024-09-11 13:21:14 +0200

    Add files via upload

---
+++ bevasarlosztori/script.js
@@ -0,0 +1,9 @@
+var bl = []
+
+function f(e) {
+    bl.push(e.value)
+    bl.sort()
+    e.value=''
+    document
+        .querySelector('#output').innerHTML = bl.map((elem, i) => `<div class="x${i % 2}"> ${elem}</div>`).join('') 
+}
